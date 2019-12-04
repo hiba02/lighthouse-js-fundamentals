b4cf531e37074d11f88907154e9afedee401a37f
hiba02 <kangyoonsoon@hotmail.com>
2019-12-04 00:34:34 +0000

    modified hello.js

--- hello.js
+++ hello.js
@@ -5,3 +5,9 @@ const sayHello = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+const sayHelloToConsole = function (name) {
+  return "Hello, " + name;
+}
+const greeting = sayHelloToConsole("John");
+console.log(greeting);
